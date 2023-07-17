2dfc8466f3bf18bcad2efcc06f433cba0c1c22d0
Lior Hadashian <lior@gavra-games.com>
2023-07-17 19:36:24 +0300

    I wanna be the very best!

--- UnityMobileRepoSummer/Assets/Scripts/PlayerMovement.cs
+++ UnityMobileRepoSummer/Assets/Scripts/PlayerMovement.cs
@@ -13,10 +13,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.forward * (10f * Time.deltaTime));
             transform.Rotate(Vector3.left * (5f * Time.deltaTime));
+            Debug.Log("Pikachu!");
         }
     }
 }
